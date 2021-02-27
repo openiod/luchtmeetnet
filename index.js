c219fdc36ba4b1ba17be88b21d171f32c9412a7d
awiel <awiel@scapeler.com>
2021-02-28 00:06:19 +0100

    init

---
+++ index.js
@@ -0,0 +1,6 @@
+
+"use strict"; // This is for your code to comply with the ECMAScript 5 standard.
+
+//var moduleLuftDatenPath = require('path').resolve(__dirname, 'node_modules/scape-luftdaten/../..');
+var luchtmeetnetStart 	= require('./luchtmeetnet-start');
+luchtmeetnetStart.start();
